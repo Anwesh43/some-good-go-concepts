18ca858ffd085ef222007ad1ca26d94cf2ced859
Anwesh43 <anweshthecool0@gmail.com>
2021-04-11 00:55:56 +0530

    using slices

--- arrayvsslices.go
+++ arrayvsslices.go
@@ -4,5 +4,9 @@ import "fmt"
 
 func main() {
 	arr := [...]string{"hello", "more", "demo", "pilage"}
-	fmt.Printf("%q", arr)
+	arr2 := []string{}
+	fmt.Printf("%q\n", arr[1:3])
+	arr2 = append(arr2, "hello")
+	fmt.Printf("%q\n", arr)
+	fmt.Printf("%q\n", arr2)
 }
